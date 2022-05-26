726c39fe0153a0ede7ace2d63b8ef609afb3f25a
aleksvelikii <aleks1397@mail.ru>
2022-05-26 16:14:06 +0300

    1

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,31 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    private static final UserService userService = new UserServiceImpl();
+    private static final User user1 = new User("Green", "Been", (byte) 13);
+    private static final User user2 = new User("Sava", "Ivanov", (byte) 24);
+    private static final User user3 = new User("Piter", "Parker", (byte) 14);
+    private static final User user4 = new User("Jon", "Connor", (byte) 72);
+
+    public static void main(String[] args) {
+        userService.createUsersTable();
+
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+    }
+}
